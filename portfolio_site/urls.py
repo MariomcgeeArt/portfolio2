36d641765cad304cfbdd5a5945a5da4b141933d3
Mario McGee <mario.mcgee@students.makeschool.com>
2020-06-05 01:04:30 -0700

    finished messageboard app and home page welcome view

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 import portfolio
+import messageboard
 
 urlpatterns = [
     path('portfolio/', include('portfolio.urls')),
+    path('messages/', include('messageboard.urls')),
     path('admin/', admin.site.urls),
 ]
