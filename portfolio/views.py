a165eca0bfc7f54e1183c38ad5c8bb984c07a5d0
Mario McGee <mario.mcgee@students.makeschool.com>
2020-06-05 00:55:18 -0700

    finisehd customized greeting

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,15 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Mario's Portfolio.")
+    #returns Home page with Marios Profile
 
 
 def contact(request):
-    return HttpResponse("Message Me.")
+    return HttpResponse("Conact Me.")
+    #returne page with text Contact  Me
+
+
+def greet_by_name(request, name):
+    return HttpResponse("<h1> Welcome, {} !</h1>".format(name))
+  # Return an HttpResponse that contains a string that includes the given name
+
